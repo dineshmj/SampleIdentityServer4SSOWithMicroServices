0277f6a689306f4895ccf9855898bbcd1fe66f28
Dinesh Muciliath Jayadevan <dineshmj@outlook.com>
2021-08-01 11:44:16 +0530

    Double registration in IoC removed.

--- CMS.IDS4/IdentityServerBuilderExtensions.cs
+++ CMS.IDS4/IdentityServerBuilderExtensions.cs
@@ -4,25 +4,17 @@ using CMS.IDP.App.Business;
 
 namespace CMS.IDP.App
 {
-	/// <summary>
-	/// Helps connect this IDP with the custom DB-based user store of the list of
-	/// applications protected by this IDP.
-	/// </summary>
+	// Helps connect this IDP with the custom DB-based user store of the list of
+	// applications protected by this IDP.
 	public static class IdentityServerBuilderExtensions
 	{
-		/// <summary>
-		/// ↓↓ ⮦⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺ SUPER CATCH: This is your own class that provides extension to the
-		/// IIdentityServiceBuilder abstraction (used by the StartUp class).
-		/// 
-		/// Connects this IDP with the custom user store from where profiles of the authenticated
-		/// users would be fetched.
-		/// </summary>
-		/// <param name="builder">The identity server builder.</param>
-		/// <returns></returns>
+		// ↓↓ ⮦⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺ SUPER CATCH: This is your own class that provides extension to the
+		// IIdentityServiceBuilder abstraction (used by the StartUp class).
+		// 
+		// Connects this IDP with the custom user store from where profiles of the authenticated
+		// users would be fetched.
 		public static IIdentityServerBuilder AddCmsUserStore (this IIdentityServerBuilder builder)
 		{
-			builder.Services.AddScoped<ICmsAuthenticationBizFacade, CmsAuthenticationBizFacade> ();
-
 			// ↓↓ ⮦⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺ SUPER CATCH: This is an IProfileService concrete class that you wrote.
 			// The IDP would use your concrete class when it requires to get user profile related services.
 			builder.AddProfileService<CmsUserProfileService> ();
